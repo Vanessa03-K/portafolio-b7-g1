d8aee01fdc4f613b23fadd4be55baab7d3395af5
Vanessa03-K <rosaura.vcch@gmail.com>
2024-08-01 11:53:41 -0600

    Mi nombre se reescribe con las cosas que hago

---
+++ script.js
@@ -0,0 +1,24 @@
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+
+typewriter
+  .pauseFor(1000)
+  .typeString('Rosaura Castillo')
+  .pauseFor(200)
+  .deleteChars(30)
+  .typeString('Desarrolladora Web')
+  .pauseFor(200)
+  .deleteChars(30)
+  .typeString('Dibujante')
+  .pauseFor(100)
+  .deleteChars(50)
+  .typeString('Ilustradora')
+  .pauseFor(200)
+  .deleteChars(50)
+  .typeString('Repostera novata')
+  .pauseFor(500)
+  .start();
